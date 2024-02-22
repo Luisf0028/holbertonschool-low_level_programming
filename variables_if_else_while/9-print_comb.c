4f9332360f5d4bc6c56c5fbfceedf662b1c4d442
lfr28 <lfrodrig12@gmail.com>
2024-02-22 15:49:43 -0400

    numeros con espacio y coma

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		putchar(y);
+		if (y < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
