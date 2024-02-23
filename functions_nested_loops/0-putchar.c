67a9d94092c67a47865540d8b10e6041e6d61d75
lfr28 <lfrodrig12@gmail.com>
2024-02-23 19:40:46 -0400

    #0 _putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <main.h>
+#include "main.h"
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Succes)
  */
+
 int main(void)
 {
-
-	putchar('_putchar')	
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
 }
