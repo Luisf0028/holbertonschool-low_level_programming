6b760201dcead4e913a5f0f4cae3da5c3f5c816a
lfr28 <lfrodrig12@gmail.com>
2024-02-25 15:46:26 -0400

    2. 10 x alphabet con betty arreglado

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
-* print_alphabet_10 - print the alphabest 10 times
-*
-* Return: void
-*/
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
+ */
 
 void print_alphabet_x10(void)
 {
 	char x;
 	int y = 0;
 
-		while (y <= 9)
-		{
+	while (y <= 9)
+	{
 		for (x = 'a'; x <= 'z'; x++)
 		{
 			_putchar(x);
 		}
-			_putchar('\n');
-			y++;
-		}
+		_putchar('\n');
+		y++;
+	}
 }
